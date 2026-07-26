8b06342c9378ee4305aa1549f6b0c738ca7d2b7f
JoselineFonseca <fonsecajoseline2@gmail.com>
2026-07-26 12:49:14 -0600

    Capa de modelo, con sus respectivas clases

---
+++ src/Modelo/DetallePedido.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Joseline
+ */
+public class DetallePedido {
+    
+    private int idDetalle;
+    private int idPedido;
+    private int idProducto;
+    private int cantidad;
+    private double precio;
+
+    public DetallePedido() {
+    }
+
+    public DetallePedido(int idDetalle, int idPedido, int idProducto, int cantidad, double precio) {
+        this.idDetalle = idDetalle;
+        this.idPedido = idPedido;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public int getIdDetalle() {
+        return idDetalle;
+    }
+
+    public void setIdDetalle(int idDetalle) {
+        this.idDetalle = idDetalle;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Detalle #" + idDetalle;
+    }
+}
